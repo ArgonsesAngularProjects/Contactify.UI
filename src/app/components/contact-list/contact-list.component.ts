ed48366a23a77362967091fb24fe5668531631ea
resr <vaclavsr@post.cz>
2023-09-14 20:56:38 +0200

    Added list component, connection to .NET and services

---
+++ src/app/components/contact-list/contact-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Contact } from 'src/app/models/Contact';
+import { ContactService } from 'src/app/services/contact.service';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent {
+  contacts: Contact[] = [];
+
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit(): void {
+    this.loadContacts();
+  }
+
+  loadContacts(): void {
+    this.contactService.getContacts().subscribe((data: Contact[]) => {
+      this.contacts = data;
+    });
+  }
+}
---
+++ src/app/models/Contact.ts
@@ -0,0 +1,10 @@
+export class Contact {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    phoneNumber?: number;
+    email?: string;
+    profession?: string;
+    city?: string;
+  }
+  
